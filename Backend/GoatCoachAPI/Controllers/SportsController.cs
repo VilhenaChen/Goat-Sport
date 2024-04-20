b1c91b788f57dca5f7edfec2950fa43ff4eacb02
DIOGO <diogodevilhena@sapo.pt>
2024-04-20 16:38:57 +0100

    Added Sports Repository

--- Backend/GoatCoachAPI/Controllers/SportsController.cs
+++ Backend/GoatCoachAPI/Controllers/SportsController.cs
@@ -9,6 +9,7 @@ using GoatCoachAPI.Data;
 using GoatCoachAPI.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using GoatCoachAPI.ViewModels;
+using GoatCoachAPI.Contracts;
 
 namespace GoatCoachAPI.Controllers
 {
@@ -18,23 +19,26 @@ namespace GoatCoachAPI.Controllers
     public class SportsController : ControllerBase
     {
         private readonly DataContext _context;
+        private readonly ISportRepository sportRepository;
 
-        public SportsController(DataContext context)
+        public SportsController(DataContext context, ISportRepository _sportRepository)
         {
             _context = context;
+            sportRepository = _sportRepository;
         }
 
         // GET: /Sports
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<SportVM>>> GetSports()
+        public async Task<ActionResult<List<GetSportsVM>>> GetSports()
         {
-            return await _context.Sports
-             .Select(s => new SportVM
+            var sports = await sportRepository.GetAllAsync();
+
+            return sports.Select(s => new GetSportsVM
              {
                  Id = s.Id,
                  Name = s.Name
              })
-             .ToListAsync();
+             .ToList();
         }
 
         // GET: /Sports/5
