a59f1308ff8cdc5cdafefb782050409068df81e1
DIOGO <diogodevilhena@sapo.pt>
2024-04-20 11:34:57 +0100

    Endpoint for getting all Sports

---
+++ Backend/GoatCoachAPI/Controllers/SportsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GoatCoachAPI.Data;
+using GoatCoachAPI.Data.Models;
+using GoatCoachAPI.Data.DTO;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GoatCoachAPI.Controllers
+{
+    [Authorize]
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SportsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SportsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Sports
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SportDto>>> GetSports()
+        {
+            return await _context.Sports
+             .Select(s => new SportDto
+             {
+                 Id = s.Id,
+                 Name = s.Name
+             })
+             .ToListAsync();
+        }
+
+        // GET: /Sports/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sport>> GetSport(int id)
+        {
+            var sport = await _context.Sports.FindAsync(id);
+
+            if (sport == null)
+            {
+                return NotFound();
+            }
+
+            return sport;
+        }
+    }
+}
---
+++ Backend/GoatCoachAPI/Data/DTO/SportDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GoatCoachAPI.Data.DTO
+{
+    public class SportDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
