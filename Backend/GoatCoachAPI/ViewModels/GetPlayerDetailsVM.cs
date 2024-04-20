276dd4e5e7e43141a996414b63b3b5f64f4f4b45
DIOGO <diogodevilhena@sapo.pt>
2024-04-20 23:00:36 +0100

    Endpoint GetPlayerDetails

---
+++ Backend/GoatCoachAPI/ViewModels/GetPlayerDetailsVM.cs
@@ -0,0 +1,14 @@
+﻿namespace GoatCoachAPI.ViewModels
+{
+
+    // Structure for the response of the Get Player Details
+    public class GetPlayerDetailsVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age {  get; set; }
+        public string Position { get; set; }
+        public int SportId { get; set; }
+
+    }
+}
